375156a9691bd741aa5be9d25ff5baab27529345
Chung Le Van <levanchung.it@gmail.com>
2023-12-11 15:47:58 +0700

    upd: build onrender successfully

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Response } from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user";
 import postRouter from "./routes/post";
