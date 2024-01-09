fa941e61cace74306d6a4dd895b868a6c601bac7
Chung Le Van <levanchung.it@gmail.com>
2024-01-09 17:32:41 +0700

    upd: getPosts user limit, page

--- src/controllers/post/get_posts.ts
+++ src/controllers/post/get_posts.ts
@@ -1,43 +1,34 @@
 import { Request, Response } from "express";
 import Post from "../../models/post";
-import Category from "models/category";
 
 const getPosts = async (req: Request, res: Response) => {
   try {
     const page = parseInt(req.query.page as string) || 1;
     const limit = parseInt(req.query.limit as string) || 10;
-    const sort = (req.query.sort as string) || "created_at";
+    const sortDirection = (req.query.sort as string) || "asc";
     const startIndex = (page - 1) * limit;
     const total = await Post.countDocuments();
 
+    let sortQuery = {};
+    if (sortDirection === "asc") {
+      sortQuery = { create_at: 1 };
+    } else if (sortDirection === "desc") {
+      sortQuery = { create_at: -1 };
+    }
+
     const posts = await Post.find()
-      .sort(sort)
+      .sort(sortQuery)
       .limit(limit)
       .skip(startIndex)
       .populate("categories")
-      .lean();
+      .lean()
+      .select("-_id -created_at -update_at -__v -status -note -author");
 
-    const postsWithCategoryNames = await Promise.all(
-      posts.map(async (post) => {
-        const categories = await Category.find({
-          _id: { $in: post.categories },
-        });
-        const categoryNames = categories.map((category) => category.name);
-
-        const cleanedContent = post.content.replace(/\n/g, "");
-
-        return {
-          ...post,
-          category_names: categoryNames,
-          content: cleanedContent,
-        };
-      })
-    );
     const results = {
       total: total,
       page: page,
       limit: limit,
-      results: postsWithCategoryNames,
+      results: posts,
     };
 
     return res.json(results);
